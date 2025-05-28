0e53068d14c1d71d680db1b48db7d68e70a124c0
Wan Sim <0420syj@naver.com>
2025-05-29 03:50:48 +0900

    fix: Icon 컴포넌트 export 추가

--- src/components/ui/icon/icon.stories.ts
+++ src/components/ui/icon/icon.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/nextjs";
-import { Icon } from "./icon";
+import { Icon } from ".";
 import { iconKeyList } from "./icon-mapper";
 
 const iconMapping = iconKeyList.reduce(

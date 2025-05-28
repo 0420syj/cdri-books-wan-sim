9c39d4047f30dbd798b375f824b61cdc10e45852
Wan Sim <0420syj@naver.com>
2025-05-29 05:36:07 +0900

    feat: 도서 검색 초기 화면 구현

---
+++ src/app/_components/book-search-result.tsx
@@ -0,0 +1,11 @@
+import { Icon } from "@/components/ui/icon";
+import { Caption } from "@/components/ui/typography";
+
+export default function BookSearchResult() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <Icon icon="icon-book" size={80} />
+      <Caption className="text-text-secondary">검색된 결과가 없습니다.</Caption>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+import { Button } from "@/components/ui/button";
+import { Caption, Title2 } from "@/components/ui/typography";
+import BookSearchResult from "./_components/book-search-result";
+
 export default function BookSearchPage() {
   return (
-    <div>
-      <h1>검색</h1>
+    <div className="p-4">
+      <Title2 className="text-[#1A1E27]">도서 검색</Title2>
+      <div className="flex gap-4">
+        <input placeholder="검색어를 입력하세요" />
+        <Button />
+      </div>
+      <div>
+        <Caption className="text-text-primary">도서 검색 결과</Caption>
+        <div className="flex">
+          <Caption className="text-text-primary">총&nbsp;</Caption>
+          <Caption className="text-palette-primary">0</Caption>
+          <Caption className="text-text-primary">건</Caption>
+        </div>
+      </div>
+      <BookSearchResult />
     </div>
   );
 }

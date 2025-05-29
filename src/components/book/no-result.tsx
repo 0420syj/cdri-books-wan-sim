50b6b9eb279672cd8f60e7d37193c87d79825790
Wan Sim <0420syj@naver.com>
2025-05-29 09:06:30 +0900

    fix: 검색 결과 없음 UI 수정

--- src/components/book/no-result.tsx
+++ src/components/book/no-result.tsx
@@ -3,7 +3,7 @@ import { Caption } from "@/components/ui/typography";
 
 export default function NoResult() {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center gap-5">
       <Icon icon="icon-book" size={80} />
       <Caption className="text-text-secondary">검색된 결과가 없습니다.</Caption>
     </div>

0f5d3ba2567d6614a63d114a5ffe2bbdb4383463
Wan Sim <0420syj@naver.com>
2025-05-29 10:12:45 +0900

    feat: SEO 최적화

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from "next";
 import BookSearchPageClient from "@/components/book/book-search-page-client";
 
 export const metadata: Metadata = {
-  title: "도서 검색",
+  title: {
+    absolute: "도서 검색 | CERTICOS BOOKS",
+  },
 };
 
 export default function BookSearchPage() {
--- src/app/wishlists/page.tsx
+++ src/app/wishlists/page.tsx
@@ -1,4 +1,9 @@
+import type { Metadata } from "next";
 import BookWishlistPageClient from "@/components/book/book-wishlist-page-client";
+
+export const metadata: Metadata = {
+  title: "내가 찜한 책",
+};
 
 export default function BookWishlistPage() {
   return <BookWishlistPageClient />;
